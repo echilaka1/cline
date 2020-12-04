8965b576cc8b7991f0e35b904566f37990d72996
Emmanuel Chilaka <emmanuelchilaka779@gmail.com>
2020-12-04 18:59:23 +0100

    update

--- src/components/Dashboard/nav.jsx
+++ src/components/Dashboard/nav.jsx
@@ -3,6 +3,15 @@ import "./nav.css";
 import edtwist from "./images/logoedtwist.png";
 
 export class NavComponent extends React.Component {
+  constructor() {
+    super();
+    this.navLinks = React.createRef();
+  }
+
+  menuAnimation = () => {
+    this.navLinks.current.classList.toggle("Navbar__ToggleShow");
+  };
+
   render() {
     return (
       <div>
@@ -11,31 +20,41 @@ export class NavComponent extends React.Component {
             <div className="Navbar__Link Navbar__Link-brand">
               <img src={edtwist} alt="Logo" className="home-logo" />
             </div>
-            <div className="Navbar__Link Navbar__Link-toggle">
+            <div
+              className="Navbar__Link Navbar__Link-togglenav"
+              onClick={this.menuAnimation}
+            >
               <i className="fa fa-bars" />
             </div>
-            <nav className="Navbar__Items">
+            <nav className="Navbar__Items" ref={this.navLinks}>
               <div className="Navbar__Link">
-                <input type="search" className="nav-search" placeholder="Generation Y" />
+                <input
+                  type="search"
+                  className="nav-search"
+                  placeholder="Generation Y"
+                />
               </div>
-              <div className="Navbar__Link">
+              <div className="Navbar__Link" onClick={this.menuAnimation}>
                 <span>Help</span>
               </div>
             </nav>
-            <nav className="Navbar__Items Navbar__Items--right">
-              <div className="Navbar__Link">
+            <nav
+              className="Navbar__Items Navbar__Items--right"
+              ref={this.navLinks}
+            >
+              <div className="Navbar__Link" onClick={this.menuAnimation}>
                 <span>
                   <i className="fa fa-bolt"></i>
                 </span>
               </div>
-              <div className="Navbar__Link">
+              <div className="Navbar__Link" onClick={this.menuAnimation}>
                 <span>
                   <span className="circular">
                     <i className="fa fa-plus"></i>
                   </span>
                 </span>
               </div>
-              <div className="Navbar__Link">
+              <div className="Navbar__Link" onClick={this.menuAnimation}>
                 <span>
                   <span className="circular black-bg">
                     <span className="item">F</span>
